1bb5eb73c38fc51495a36f9873fe6bd3e2c68efc
“fly” <809176334@qq.com>
2024-11-13 17:31:37 +0800

    FLYBluetoothHandler的回调参数增加nullable描述，去掉传nil时的警告

--- BluetoothDemo/FLYBluetoothHandler.m
+++ BluetoothDemo/FLYBluetoothHandler.m
@@ -79,7 +79,7 @@ typedef NS_ENUM(NSInteger, FLYCommandType) {
 
 
 /// 往特征里写入数据
-- (void)bluetoothWriteWithDeviceName:(NSString *)name data:(NSData *)data characteristicUUID:(NSString *)characteristicUUID success:(void (^)(void))success failure:(nullable BLEFailureBlock)failure progress:(nullable BLEProgressBlock)progress
+- (void)bluetoothWriteWithDeviceName:(NSString *)name data:(NSData *)data characteristicUUID:(NSString *)characteristicUUID success:(nullable void (^)(void))success failure:(nullable BLEFailureBlock)failure progress:(nullable BLEProgressBlock)progress
 {
     // 写入成功的block是没有参数的，而self.success是有参数的，赋值会报错，所以需要强转一下。
     self.success = (void (^)(id))success;
